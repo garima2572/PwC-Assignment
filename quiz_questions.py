99e9f7e09a2a95b8d7406b9db56f9f0f5ebedc2c
garima2572 <154080430+garima2572@users.noreply.github.com>
2023-12-17 21:19:52 +0400

    Update quiz_questions.py

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,8 +1,8 @@
-import openai
+#import openai
 
-openai.api_key = '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'
+#openai.api_key = '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'
 
-def generate_quiz(topic):
+#def generate_quiz(topic):
     prompt = f"Create a multiple-choice quiz about {topic}. Questions:\n1."
     response = openai.ChatCompletion.create(
         model="text-davinci-003",
