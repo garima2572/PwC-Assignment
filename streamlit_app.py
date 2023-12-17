953c767d6ba38cf9cbd6f5899ac360f3b0d2d56e
garima2572 <154080430+garima2572@users.noreply.github.com>
2023-12-17 21:18:15 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,66 @@
 import streamlit as st
+import openai
 
-# Function to display the generated quiz questions
-def display_quiz(questions):
-    st.markdown(f"## Quiz Questions\n{questions}")
+# Set up OpenAI API credentials
+openai.api_key = "YOUR_API_KEY"
 
-def main():
-    st.title("MCQ Quiz Generator")
+def generate_quiz(topic: str):
+    """
+    Generates an AI-powered multiple choice quiz using langchain and streamlit.
 
-    # User input for topic
-    topic = st.text_input("Enter the topic for the quiz:")
+    Parameters:
+    - topic: str
+        The topic for which the quiz is generated.
 
-    if st.button("Generate Quiz"):
-        if topic:
-            quiz_questions = generate_quiz(topic)
-            display_quiz(quiz_questions)
-        else:
-            st.warning("Please enter a topic.")
+    Returns:
+    - None
+        The function displays the generated quiz and the user's score.
 
-if __name__ == "__main__":
-    main()
+    Raises:
+    - ValueError:
+        Raises an error if the topic is not provided.
+    """
+
+    # Validate the topic
+    if not topic:
+        raise ValueError("Please provide a topic for the quiz.")
+
+    # Generate quiz using OpenAI Chat Completion API
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Generate a multiple choice quiz on {topic}.",
+        max_tokens=100,
+        n=5,
+        stop=None,
+        temperature=0.7
+    )
+
+    # Extract quiz questions from the API response
+    questions = response.choices[0].text.strip().split("\n")
+
+    # Display the quiz using Streamlit
+    st.title("AI-powered Multiple Choice Quiz")
+    st.write(f"Topic: {topic}")
+    st.write("Instructions: Select the correct option for each question.")
+    st.write("")
+
+    # Initialize score
+    score = 0
+
+    # Display each question and get user's answer
+    for i, question in enumerate(questions):
+        st.write(f"Question {i+1}: {question}")
+        options = ["Option A", "Option B", "Option C", "Option D"]
+        user_answer = st.radio("Select your answer:", options)
+        correct_answer = "Option A"  # Assuming Option A is always the correct answer
+
+        # Check if user's answer is correct
+        if user_answer == correct_answer:
+            score += 1
+
+    # Display the user's score
+    st.write("")
+    st.write(f"Your score: {score}/{len(questions)}")
+
+# Example usage
+generate_quiz("Python Programming")
