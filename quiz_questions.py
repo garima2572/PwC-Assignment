aefbc0d6567cb419199b14b9f3e4299717ccfb96
garima2572 <154080430+garima2572@users.noreply.github.com>
2023-12-18 14:33:10 +0400

    Update quiz_questions.py

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,23 +1,24 @@
 import langchain
 import openai
+import os
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
 # Set your OpenAI API key
 openai.api_key = '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'
+os.environ["OPENAI_API_KEY"] = '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'
 
 # Function to generate quiz questions and answers using Langchain and OpenAI's Chat Completion API
 def generate_quiz(topic, num_questions):
     pipeline = langchain.Pipeline("openai/text-davinci-003")
 
     # Generate questions based on the topic using Langchain
-    prompt = f"Create a multiple-choice quiz about {topic}. Questions:"
-    result = pipeline(prompt, num_outputs=num_questions)
+    template = f"Create a multiple-choice quiz about {topic}. Questions:"
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm = OpenAI()
 
-    quiz_data = []
-    for item in result:
-        question = item['text']
-        # Generate answer options for each question
-        # Implement logic to format questions and answer options as needed
-        # Identify the correct answer for each question
-        # Append formatted question, answer options, and correct answer to quiz_data list
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    results = [llm_chain.run(question) for i in range(num_qestions)]
 
-    return quiz_data
+    return results
