3e2b39d9ecc790ed19b8543e256b624c02df69b3
garima2572 <154080430+garima2572@users.noreply.github.com>
2023-12-18 15:53:47 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,27 +9,24 @@ def main():
     topic = st.text_input("Enter the topic for the quiz:")
     num_questions = st.number_input("Number of questions:", min_value=1, max_value=10, value=5, step=1)
 
-    if st.button("Start Quiz"):
-        if topic:
-            # Generate quiz questions and answers
-            quiz_data = generate_quiz(topic, num_questions)
+    # Generate quiz questions and answers
+    quiz_data = generate_quiz(topic, num_questions)
 
-            question_options = []
-            for i, question_data in enumerate(quiz_data):
-                st.markdown(f"### Question {i + 1}: {question_data['question']}")
-                radio = st.radio(
-                            "Select the most appropriate option",
-                            ["A.", "B.", "C.", "D."],
-                            captions = [question_data['option_a'], question_data['option_b'],
-                                       question_data['option_c'], question_data['option_d']], key = f'ques_{i}')
-                question_options.append(radio)
+    question_options = []
+    for i, question_data in enumerate(quiz_data):
+        st.markdown(f"### Question {i + 1}: {question_data['question']}")
+        radio = st.radio(
+                    "Select the most appropriate option",
+                    ["A.", "B.", "C.", "D."],
+                    captions = [question_data['option_a'], question_data['option_b'],
+                               question_data['option_c'], question_data['option_d']], key = f'ques_{i}')
+        question_options.append(radio)
 
-                if radio == question_data["correct_option"]:
-                    st.write('Correct option.')
-                else:
-                    st.write('Incorrect option.')
+        if radio == question_data["correct_option"]:
+            st.write('Correct option.')
         else:
-            st.warning("Please enter a topic to start the quiz.")
+            st.write('Incorrect option.')
+
 
 if __name__ == "__main__":
     main()
