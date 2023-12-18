c4a07ecf0daf1054e2f7a536af5d833746b5e46b
garima2572 <154080430+garima2572@users.noreply.github.com>
2023-12-18 14:48:17 +0400

    Update quiz_questions.py

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,20 +1,16 @@
-import langchain
-import openai
-import os
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
 # Set your OpenAI API key
-openai.api_key = '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'
-os.environ["OPENAI_API_KEY"] = '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'
+API_KEY = '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'
 
 # Function to generate quiz questions and answers using Langchain and OpenAI's Chat Completion API
 def generate_quiz(topic, num_questions):
     # Generate questions based on the topic using Langchain
     template = "Question: {question}"
     prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm = OpenAI()
+    llm = OpenAI(openai_api_key=API_KEY)
 
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     question = f"Create a multiple-choice quiz about {topic}."
