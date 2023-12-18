56ed30f1e4d8d12f6e3600d0e6d18b1080117bc1
garima2572 <154080430+garima2572@users.noreply.github.com>
2023-12-18 09:11:18 +0400

    Update quiz_questions.py

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,16 +1,23 @@
-#import openai
+import langchain
+import openai
 
-#openai.api_key = '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'
+# Set your OpenAI API key
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-#def generate_quiz(topic):
-    #prompt = f"Create a multiple-choice quiz about {topic}. Questions:\n1."
-    #response = openai.ChatCompletion.create(
-     #   model="text-davinci-003",
-      #  messages=[
-       #     {"role": "system", "content": prompt}
-       # ],
-       # max_tokens=150
-   # )
-    # Extracting generated quiz questions from the OpenAI response
-    #quiz_questions = response['choices'][0]['message']['content']
-    #return quiz_questions
+# Function to generate quiz questions and answers using Langchain and OpenAI's Chat Completion API
+def generate_quiz(topic, num_questions):
+    pipeline = langchain.Pipeline("openai/text-davinci-003")
+
+    # Generate questions based on the topic using Langchain
+    prompt = f"Create a multiple-choice quiz about {topic}. Questions:"
+    result = pipeline(prompt, num_outputs=num_questions)
+
+    quiz_data = []
+    for item in result:
+        question = item['text']
+        # Generate answer options for each question
+        # Implement logic to format questions and answer options as needed
+        # Identify the correct answer for each question
+        # Append formatted question, answer options, and correct answer to quiz_data list
+
+    return quiz_data
